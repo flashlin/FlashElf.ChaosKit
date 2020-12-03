ed004eb12befaaec8c9531cc4e6f38c1892619f3
flashlin <flash.lin@titansoft.com.sg>
2020-12-03 18:11:24 +0800

    add JsonSerializer

---
+++ FlashElf.ChaosKit/ChaosJsonSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using T1.Standard.DynamicCode;
+using T1.Standard.Extensions;
+
+namespace FlashElf.ChaosKit
+{
+	public class ChaosJsonSerializer : IChaosSerializer
+	{
+		public byte[] Serialize<T>(T obj)
+		{
+			return JsonSerializer.Serialize(obj).ToByteArray();
+		}
+
+		public object Deserialize(Type type, byte[] data)
+		{
+			var deserialize = DynamicMethod.GetGenericMethod(
+				typeof(JsonSerializer),
+				new[] { type },
+				nameof(JsonSerializer.Deserialize),
+				new Type[] { typeof(string), typeof(JsonSerializerOptions) });
+			var json = Encoding.ASCII.GetString(data);
+			return deserialize(null, new object[] { json });
+		}
+	}
+}
