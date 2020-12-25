227fa81e9f0cbd6b9975974ae72339e091cdaeda
flashlin <flash.lin@gmail.com>
2020-12-25 20:23:34 +0800

    upgrade to PackageReference

--- pack.ps1
+++ pack.ps1
@@ -1,5 +1,10 @@
 Remove-Item .\nupkgs\*.nupkg
 & dotnet pack .\FlashElf.ChaosKit --output nupkgs
-& nuget pack -Build .\FlashElf.ChaosKit.Autofac\FlashElf.ChaosKit.Autofac.csproj `
-    -IncludeReferencedProjects `
-    -OutputDirectory nupkgs
+
+# & nuget pack -Build .\FlashElf.ChaosKit.Autofac\FlashElf.ChaosKit.Autofac.csproj `
+#     -IncludeReferencedProjects `
+#     -OutputDirectory nupkgs
+
+$msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64\MSBuild.exe"
+& $msbuild .\FlashElf.ChaosKit.Autofac\FlashElf.ChaosKit.Autofac.csproj `
+    -t:pack
