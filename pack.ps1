0c0b0a06bf22d0b9225591e264cb418ccf617754
flashlin <flash.lin@titansoft.com.sg>
2020-12-28 23:12:13 +0800

    fix pack

--- pack.ps1
+++ pack.ps1
@@ -5,6 +5,13 @@ Remove-Item .\nupkgs\*.nupkg
 #     -IncludeReferencedProjects `
 #     -OutputDirectory nupkgs
 
-$msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64\MSBuild.exe"
+$msbuild1 = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64\MSBuild.exe"
+$msbuild2 = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\amd64\MSBuild.exe"
+
+$msbuild = $msbuild2
+if( [System.IO.File]::Exists($msbuild1) ) {
+    $msbuild = $msbuild1
+}
+
 & $msbuild .\FlashElf.ChaosKit.Autofac\FlashElf.ChaosKit.Autofac.csproj `
     -t:pack
