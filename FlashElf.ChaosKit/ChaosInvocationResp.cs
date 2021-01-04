45cff552faedc55d9a4cdc42c15fe269fcf201e7
flashlin <flash.lin@titansoft.com.sg>
2021-01-04 10:29:31 +0800

    add toString

--- FlashElf.ChaosKit/ChaosInvocationResp.cs
+++ FlashElf.ChaosKit/ChaosInvocationResp.cs
@@ -8,5 +8,27 @@ namespace FlashElf.ChaosKit
 		public string DataTypeFullName { get; set; }
 		public byte[] Data { get; set; }
 		public SerializeException Exception { get; set; }
+
+		public override string ToString()
+		{
+			if (Data == null || Data.Length == 0)
+			{
+				return "(null)";
+			}
+
+			object value = null;
+			try
+			{
+				var converter = new ChaosConverter(new ChaosBinarySerializer());
+				value = converter.ToData(this);
+			}
+			catch
+			{
+				var converter = new ChaosConverter(new ChaosJsonSerializer());
+				value = converter.ToData(this);
+			}
+
+			return $"{value}";
+		}
 	}
 }
